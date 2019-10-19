860e2dbc30a89a84edf0c44095dbde825cc0b16e
jamesnyakush <emvesttech@gmail.com>
2019-10-19 22:38:29 +0300

    added login and register

---
+++ app/src/main/java/com/jamesnyakush/employerselfservice/view/Login.kt
@@ -0,0 +1,13 @@
+package com.jamesnyakush.employerselfservice.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jamesnyakush.employerselfservice.R
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
---
+++ app/src/main/java/com/jamesnyakush/employerselfservice/view/Register.kt
@@ -0,0 +1,13 @@
+package com.jamesnyakush.employerselfservice.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jamesnyakush.employerselfservice.R
+
+class Register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_activity)
+    }
+}
